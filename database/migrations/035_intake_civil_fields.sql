dc438b563de3265a1c051dc5849e969ae1e8bc39
YvanClarc <yonzonivanclark@gmail.com>
2026-04-14 20:26:11 +0800

    MGC

--- database/migrations/035_intake_civil_fields.sql
+++ database/migrations/035_intake_civil_fields.sql
@@ -8,4 +8,5 @@ USE law_system_auth;
 
 ALTER TABLE case_intake_requests
   ADD COLUMN civil_case_type VARCHAR(50)     NULL  AFTER case_type,
-  ADD COLUMN claim_amount     DECIMAL(15, 2) NULL  AFTER civil_case_type;
+  ADD COLUMN claim_amount     DECIMAL(15, 2) NULL  AFTER civil_case_type,
+  ADD COLUMN tort_case_type   VARCHAR(50)     NULL  AFTER claim_amount;
